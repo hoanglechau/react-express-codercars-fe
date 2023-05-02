6272d55288d89fd4251751293a51ddb8ac108b44
Hoang Le Chau <44614317+hoanglechau@users.noreply.github.com>
2023-05-02 12:26:14 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { BrowserRouter } from "react-router-dom";
+import Router from "./routes";
+import ThemeProvider from "./theme";
+import "./App.css";
+import { Box, CssBaseline, Typography } from "@mui/material";
+import Copyright from "./components/Copyright";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <BrowserRouter>
+      <ThemeProvider>
+        <CssBaseline />
+        <Box
+          className="hero-image"
+          mb={2}
+          display="flex"
+          justifyContent="center"
+          alignItems="center"
+        >
+          <Typography variant="h1" color="white" className="title">
+            CODERCAR DATABASE
+          </Typography>
+        </Box>
+        <Router />
+        <Copyright />
+      </ThemeProvider>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
