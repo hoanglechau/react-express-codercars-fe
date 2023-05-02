2328a880d3e1af536e2cb06f66475c97c9d78f94
Hoang Le Chau <44614317+hoanglechau@users.noreply.github.com>
2023-05-02 17:30:31 +0700

    Update FormModal.jsx

--- src/components/FormModal.jsx
+++ src/components/FormModal.jsx
@@ -1,38 +1,72 @@
-import React, { useEffect, useState } from 'react';
-import Button from '@mui/material/Button';
-import TextField from '@mui/material/TextField';
-import Dialog from '@mui/material/Dialog';
-import DialogActions from '@mui/material/DialogActions';
-import DialogContent from '@mui/material/DialogContent';
-import DialogTitle from '@mui/material/DialogTitle';
-import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns';
-import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
-import { DatePicker } from '@mui/x-date-pickers';
-import { FormControl, FormHelperText, InputLabel, MenuItem, Select, Stack } from '@mui/material';
-import apiService from '../app/apiService';
-import Joi from 'joi';
-import moment from 'moment';
+import { useEffect, useState } from "react";
+import Button from "@mui/material/Button";
+import TextField from "@mui/material/TextField";
+import Dialog from "@mui/material/Dialog";
+import DialogActions from "@mui/material/DialogActions";
+import DialogContent from "@mui/material/DialogContent";
+import DialogTitle from "@mui/material/DialogTitle";
+import { AdapterDateFns } from "@mui/x-date-pickers/AdapterDateFns";
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+import { DatePicker } from "@mui/x-date-pickers";
+import {
+	FormControl,
+	FormHelperText,
+	InputLabel,
+	MenuItem,
+	Select,
+	Stack,
+} from "@mui/material";
+import apiService from "../app/apiService";
+import Joi from "joi";
+import moment from "moment";
 
-const initial_form = { make: '', model: '', release_date: '', transmission_type: '', price: 0, size: '', style: '' };
+const initial_form = {
+	make: "",
+	model: "",
+	release_date: "",
+	transmission_type: "",
+	price: 0,
+	size: "",
+	style: "",
+};
 
-export default function FormModal({ open, handleClose, mode, selectedCar, modalKey, refreshData }) {
+export default function FormModal({
+	open,
+	handleClose,
+	mode,
+	selectedCar,
+	modalKey,
+	refreshData,
+}) {
 	const [form, setForm] = useState(initial_form);
 	const [errors, setErrors] = useState({});
 	const schema = Joi.object({
 		make: Joi.string().required(),
 		model: Joi.string().required(),
-		release_date: Joi.number().integer().min(1900).max(new Date().getFullYear()).required(),
-		transmission_type: Joi.string().valid('MANUAL', 'AUTOMATIC', 'AUTOMATED_MANUAL', 'DIRECT_DRIVE', 'UNKNOWN').required(),
+		release_date: Joi.number()
+			.integer()
+			.min(1900)
+			.max(new Date().getFullYear())
+			.required(),
+		transmission_type: Joi.string()
+			.valid(
+				"MANUAL",
+				"AUTOMATIC",
+				"AUTOMATED_MANUAL",
+				"DIRECT_DRIVE",
+				"UNKNOWN"
+			)
+			.required(),
 		price: Joi.number().integer().min(1000).required(),
-		size: Joi.string().valid('Compact', 'Midsize', 'Large').required(),
+		size: Joi.string().valid("Compact", "Midsize", "Large").required(),
 		style: Joi.string().required(),
 	}).options({ stripUnknown: true, abortEarly: false });
 
-	const handleChange = (e) => {
+	const handleChange = e => {
 		const { name, value } = e.target;
 		setForm({ ...form, [name]: value });
 	};
-	const handleEdit = async (newForm) => {
+	const handleEdit = async newForm => {
 		try {
 			await apiService.put(`/cars/${selectedCar?._id}`, { ...newForm });
 			refreshData();
@@ -40,9 +74,9 @@ export default function FormModal({ open, handleClose, mode, selectedCar, modalK
 			console.log(err);
 		}
 	};
-	const handleCreate = async (newForm) => {
+	const handleCreate = async newForm => {
 		try {
-			const res = await apiService.post('/cars', { ...newForm });
+			const res = await apiService.post("/cars", { ...newForm });
 			refreshData();
 			console.log(res);
 		} catch (err) {
@@ -53,10 +87,12 @@ export default function FormModal({ open, handleClose, mode, selectedCar, modalK
 		const validate = schema.validate(form);
 		if (validate.error) {
 			const newErrors = {};
-			validate.error.details.forEach((item) => (newErrors[item.path[0]] = item.message));
+			validate.error.details.forEach(
+				item => (newErrors[item.path[0]] = item.message)
+			);
 			setErrors(newErrors);
 		} else {
-			if (mode === 'create') handleCreate(validate.value);
+			if (mode === "create") handleCreate(validate.value);
 			else handleEdit(validate.value);
 			// handleClose();
 		}
@@ -67,7 +103,7 @@ export default function FormModal({ open, handleClose, mode, selectedCar, modalK
 			setForm(selectedCar);
 		} else setForm(initial_form);
 	}, [selectedCar]);
-	
+
 	return (
 		<LocalizationProvider dateAdapter={AdapterDateFns} key={modalKey}>
 			<Dialog
@@ -77,7 +113,9 @@ export default function FormModal({ open, handleClose, mode, selectedCar, modalK
 					setErrors({});
 				}}
 			>
-				<DialogTitle>{mode === 'create' ? 'CREATE A NEW CAR' : 'EDIT CAR'}</DialogTitle>
+				<DialogTitle>
+					{mode === "create" ? "CREATE A NEW CAR" : "EDIT CAR"}
+				</DialogTitle>
 				<DialogContent>
 					<Stack spacing={2}>
 						<TextField
@@ -106,27 +144,59 @@ export default function FormModal({ open, handleClose, mode, selectedCar, modalK
 							fullWidth
 							variant="standard"
 						/>
-						<FormControl error={errors.transmission_type} variant="standard" sx={{ m: 1, minWidth: 120 }}>
-							<InputLabel id="transmission_type_label">Transmission Type</InputLabel>
-							<Select labelId="transmission_type_label" name="transmission_type" value={form.transmission_type} onChange={handleChange} label="Transmission Type">
-								{['MANUAL', 'AUTOMATIC', 'AUTOMATED_MANUAL', 'DIRECT_DRIVE', 'UNKNOWN'].map((item) => (
+						<FormControl
+							error={errors.transmission_type}
+							variant="standard"
+							sx={{ m: 1, minWidth: 120 }}
+						>
+							<InputLabel id="transmission_type_label">
+								Transmission Type
+							</InputLabel>
+							<Select
+								labelId="transmission_type_label"
+								name="transmission_type"
+								value={form.transmission_type}
+								onChange={handleChange}
+								label="Transmission Type"
+							>
+								{[
+									"MANUAL",
+									"AUTOMATIC",
+									"AUTOMATED_MANUAL",
+									"DIRECT_DRIVE",
+									"UNKNOWN",
+								].map(item => (
 									<MenuItem value={item} key={item}>
 										{item}
 									</MenuItem>
 								))}
 							</Select>
-							{errors.transmission_type ? <FormHelperText>{errors.transmission_type}</FormHelperText> : null}
+							{errors.transmission_type ? (
+								<FormHelperText>{errors.transmission_type}</FormHelperText>
+							) : null}
 						</FormControl>
-						<FormControl error={errors.size} variant="standard" sx={{ m: 1, minWidth: 120 }}>
+						<FormControl
+							error={errors.size}
+							variant="standard"
+							sx={{ m: 1, minWidth: 120 }}
+						>
 							<InputLabel id="size-label">Size</InputLabel>
-							<Select labelId="size-label" name="size" value={form.size} onChange={handleChange} label="Size">
-								{['Compact', 'Midsize', 'Large'].map((item) => (
+							<Select
+								labelId="size-label"
+								name="size"
+								value={form.size}
+								onChange={handleChange}
+								label="Size"
+							>
+								{["Compact", "Midsize", "Large"].map(item => (
 									<MenuItem value={item} key={item}>
 										{item}
 									</MenuItem>
 								))}
 							</Select>
-							{errors.size ? <FormHelperText>{errors.size}</FormHelperText> : null}
+							{errors.size ? (
+								<FormHelperText>{errors.size}</FormHelperText>
+							) : null}
 						</FormControl>
 						<TextField
 							error={errors.style}
@@ -142,14 +212,21 @@ export default function FormModal({ open, handleClose, mode, selectedCar, modalK
 						/>
 						<Stack direction="row" spacing={2}>
 							<DatePicker
-								views={['year']}
+								views={["year"]}
 								label="Year"
-								value={moment(form.release_date.toString()).format('YYYY')}
+								value={moment(form.release_date.toString()).format("YYYY")}
 								error={errors.release_date}
-								onChange={(newValue) => {
+								onChange={newValue => {
 									setForm({ ...form, release_date: moment(newValue).year() });
 								}}
-								renderInput={(params) => <TextField {...params} helperText={errors.release_date ? errors.release_date : null} />}
+								renderInput={params => (
+									<TextField
+										{...params}
+										helperText={
+											errors.release_date ? errors.release_date : null
+										}
+									/>
+								)}
 							/>
 
 							<TextField
@@ -168,7 +245,9 @@ export default function FormModal({ open, handleClose, mode, selectedCar, modalK
 				</DialogContent>
 				<DialogActions>
 					<Button onClick={handleClose}>Cancel</Button>
-					<Button onClick={handleSubmit}>{mode === 'create' ? 'Create' : 'Save'}</Button>
+					<Button onClick={handleSubmit}>
+						{mode === "create" ? "Create" : "Save"}
+					</Button>
 				</DialogActions>
 			</Dialog>
 		</LocalizationProvider>
