61ac1615c52610d85fce71b90b16a435dd5d0b17
Hoang Le Chau <44614317+hoanglechau@users.noreply.github.com>
2023-05-02 17:33:11 +0700

    Update apiService.js

--- src/app/apiService.js
+++ src/app/apiService.js
@@ -9,7 +9,7 @@ apiService.interceptors.request.use(
     console.log("Start Request", request);
     return request;
   },
-  function (error) {
+  function(error) {
     console.log("REQUEST ERROR", { error });
     return Promise.reject(error);
   }
@@ -20,7 +20,7 @@ apiService.interceptors.response.use(
     console.log("Response", response);
     return response.data;
   },
-  function (error) {
+  function(error) {
     console.log("RESPONSE ERROR", error.response);
     const message = error.response?.data?.message || "Unknown Error";
     return Promise.reject({ message });
