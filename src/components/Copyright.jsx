1b0118cf9096c3c5023385b8930f16b917783ffb
Hoang Le Chau <44614317+hoanglechau@users.noreply.github.com>
2023-05-02 17:33:17 +0700

    Update Copyright.jsx

--- src/components/Copyright.jsx
+++ src/components/Copyright.jsx
@@ -1,12 +1,11 @@
-import { Typography } from '@mui/material';
-import React from 'react';
+import { Typography } from "@mui/material";
 
 const Copyright = () => {
 	return (
 		<Typography variant="body2" color="textSecondary" align="center">
-			{'Copyright © '}
+			{"Copyright © "}
 			CoderSchool {new Date().getFullYear()}
-			{'.'}
+			{"."}
 		</Typography>
 	);
 };
